8ee814e11b6cc3672834d2208d20246f536e587a
anais di genua <anais.digenua5@gmail.com>
2022-07-25 14:51:55 +0200

    add data project

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,30 @@
+﻿using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+
+    //Dati MOCK
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
