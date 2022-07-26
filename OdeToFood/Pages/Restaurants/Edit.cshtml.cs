ac5e680b428bf5cb40bfae61d690c1c6ef071665
anais di genua <anais.digenua5@gmail.com>
2022-07-26 11:25:19 +0200

    building edit form

--- OdeToFood/Pages/Restaurants/Edit.cshtml.cs
+++ OdeToFood/Pages/Restaurants/Edit.cshtml.cs
@@ -1,25 +1,34 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using OdeToFood.Core;
 using OdeToFood.Data;
 
 namespace OdeToFood.Pages.Restaurants
 {
-    public class DetailModel : PageModel
+    public class EditModel : PageModel
     {
         private readonly IRestaurantData restaurantData;
-        public Restaurant Restaurant { get; set; }
+        private readonly IHtmlHelper htmlHelper;
 
-        public DetailModel(IRestaurantData restaurantData)
+        //entità
+        public Restaurant Restaurant { get; set; }
+        public IEnumerable<SelectListItem> Cuisines { get; set; }
+
+        public EditModel(IRestaurantData restaurantData, IHtmlHelper htmlHelper)
         {
             this.restaurantData = restaurantData;
+            this.htmlHelper = htmlHelper;
         }
-
         public IActionResult OnGet(int restaurantId)
         {
+            //tramite questo tag helper prendo la lista di enum
+            Cuisines = htmlHelper.GetEnumSelectList<CuisineType>();
+
             //assoccio l'entità restaurant al ristorante scelto dall'utente tramite id
             Restaurant = restaurantData.GetById(restaurantId);
 
+            //se il ristorante non esiste più
             if (Restaurant == null)
                 return RedirectToPage("./NotFound");
             return Page();
